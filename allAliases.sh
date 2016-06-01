3c28aa0ba687af9e64452b98c41a694c15fc76a6
Jon Horner <hornerjn@mail.uc.edu>
2016-06-01 10:25:58 -0400

    Added error output

--- allAliases.sh
+++ allAliases.sh
@@ -7,5 +7,7 @@
 if [ -d $UTILITIESDIR ]; then
   source $UTILITIESDIR/commonAliases.sh
   source $UTILITIESDIR/gitAliases.sh
+else
+  echo "Requires a environment variable named \"UTILITIESDIR\" to exist and contain the path to this project directory's location."
+  echo $UTILITIESDIR
 fi
-
