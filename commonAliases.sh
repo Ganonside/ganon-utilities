2cd337516d37f7ede264f936e48a426fb54ac69e
Jon Horner <hornerjn@mail.uc.edu>
2016-06-01 09:45:18 -0400

    Added files

---
+++ commonAliases.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+###############################################
+# Contains aliases for commonly used commands #
+###############################################
+
+# Show hidden files in Finder
+alias showfiles='
+  defaults write com.apple.finder AppleShowAllFiles true;
+  killall Finder /System/Library/CoreServices/Finder.app;'
+
+# Hide hidden files in Finder
+alias hideFiles='
+  defaults write com.apple.finder AppleShowAllFiles false;
+  killall Finder /System/Library/CoreServices/Finder.app;'
+
+# Navigation aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
---
+++ gitAliases.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+###################################################
+# Contains aliases for commonly used git commands #
+###################################################
+
+alias ga="git add -u"
+alias gaa="git add -A"
+alias gai="git add -i"
+alias gb="git branch "
+alias gc="git commit"
+alias gch="git checkout "
+alias gchb="git checkout -b "
+alias gcm="git commit -m "
+alias gpll="git pull"
+alias gpsh="git push"
+alias gs="git status"
