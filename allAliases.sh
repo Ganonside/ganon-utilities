1030f7a47ae49d9ef50d5a5fb9bb31c34837fcef
Jonathan Horner <Ganonside@users.noreply.github.com>
2016-06-01 09:52:43 -0400

    Create allAliases.sh

---
+++ allAliases.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+###########################
+# Imports all alias files #
+###########################
+
+test "./commonAliases.sh" && source "./commonAliases.sh"
+test "./gitAliases.sh" && source "./gitAliases.sh"
